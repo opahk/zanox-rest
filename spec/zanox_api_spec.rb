9cd4c97235940c111a1a2bbf56561cf9277df4ac
Bastian Platz <mail@bastianplatz.de>
2012-03-15 12:51:41 +0100

    added test case

--- spec/zanox_api_spec.rb
+++ spec/zanox_api_spec.rb
@@ -28,4 +28,16 @@ describe "Zanox API" do
     response.method_not_there.nested_not_there.should be_nil
   end
 
+  it "processes data correctly" do
+
+      api_response = {"page"=>0, "items"=>1, "total"=>1, "saleItems"=>{"saleItem"=>[{"@id"=>"789050fb-0d5a-4987-94fa-2d294f28fbd1", "reviewState"=>"open", "trackingDate"=>"2012-02-13T00:00:00+01:00", "modifiedDate"=>"2012-02-14T10:40:05.937+01:00", "adspace"=>{"@id"=>"1640652", "$"=>"Boost"}, "admedium"=>{"@id"=>"507303", "$"=>"0 Textlink - Weihnachten"}, "program"=>{"@id"=>"1646", "$"=>"Amazon DE"}, "clickId"=>0, "clickInId"=>0, "amount"=>15.23, "commission"=>0.76, "currency"=>"EUR", "reviewNote"=>"ASIN: B0071LPES4", "trackingCategory"=>{"@id"=>"25809", "$"=>"23_ukn_Elektronik & Foto"}}]}}
+
+      response = Zanox::Response.new(api_response)
+
+      si = response.sale_items.sale_item
+      si.size.should == 1
+      si.first.click_id.should == 0
+      si.first.click_date.should be_nil
+  end
+
 end
