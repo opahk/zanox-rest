0296e93c7f4eed310176a864c6467cafbca6ef7c
Frank C. Eckert <frank.eckert@boost-project.com>
2012-12-30 22:11:10 +0100

    update specs

--- spec/zanox_api_spec.rb
+++ spec/zanox_api_spec.rb
@@ -6,13 +6,16 @@ CONFIG = YAML.load_file('spec/zanox_data.yml')
 
 describe "Zanox API" do
 
-  it "establishes a connection to zanox api and gets basic report for a month in the past, checks it for sanety" do
-    report = Zanox::Report.basic(Date.parse(CONFIG['basic']['fromdate']), Date.parse(CONFIG['basic']['todate']))
+  it "establishes a connection to zanox api and gets basic report for a month
+  in the past, checks it for sanety" do
+    report = ZanoxAPI::Report.basic(
+      Date.parse(CONFIG['basic']['fromdate']),
+      Date.parse(CONFIG['basic']['todate']))
     report.total.should == CONFIG['basic']['total']
   end
 
   it "establishes a connection to zanox api and gets one hopefully correct dataset" do
-    report = Zanox::Report.sales(Date.parse(CONFIG['sales']['date']))
+    report = ZanoxAPI::Report.sales(Date.parse(CONFIG['sales']['date']))
     report.items.should == CONFIG['sales']['items']
     report.sale_items.sale_item.first.id.should == CONFIG['sales']['firstid']
     report.sale_items.sale_item.first.program.value.should == CONFIG['sales']['firstprogram']
@@ -21,7 +24,7 @@ describe "Zanox API" do
 
   it "accepts abitrarily nested method invokations on response object" do
 
-    response = Zanox::Response.new({:a => 1, :b => 2})
+    response = ZanoxAPI::Response.new({:a => 1, :b => 2})
     response.a.should == 1
     response.b.should == 2
     response.method_not_there.should be_nil
@@ -32,12 +35,11 @@ describe "Zanox API" do
 
       api_response = {"page"=>0, "items"=>1, "total"=>1, "saleItems"=>{"saleItem"=>[{"@id"=>"789050fb-0d5a-4987-94fa-2d294f28fbd1", "reviewState"=>"open", "trackingDate"=>"2012-02-13T00:00:00+01:00", "modifiedDate"=>"2012-02-14T10:40:05.937+01:00", "adspace"=>{"@id"=>"1640652", "$"=>"Boost"}, "admedium"=>{"@id"=>"507303", "$"=>"0 Textlink - Weihnachten"}, "program"=>{"@id"=>"1646", "$"=>"Amazon DE"}, "clickId"=>0, "clickInId"=>0, "amount"=>15.23, "commission"=>0.76, "currency"=>"EUR", "reviewNote"=>"ASIN: B0071LPES4", "trackingCategory"=>{"@id"=>"25809", "$"=>"23_ukn_Elektronik & Foto"}}]}}
 
-      response = Zanox::Response.new(api_response)
+      response = ZanoxAPI::Response.new(api_response)
 
       si = response.sale_items.sale_item
       si.size.should == 1
       si.first.click_id.should == 0
       si.first.click_date.should be_nil
   end
-
 end
